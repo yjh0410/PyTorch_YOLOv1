e54bf14fde300b6113446fb5277c36174c1c49a8
yjh0410 <1394571815@qq.com>
2022-12-07 13:21:42 +0800

    gradient accumulate

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@ python train.py \
         -d coco \
         -ms \
         -bs 16 \
-        -accu 4
+        -accu 4 \
         --lr 0.001 \
         --max_epoch 150 \
         --lr_epoch 90 120 \
